d48fbebb2185dfe972f76e699063eb2c4019ef97
suntwister <s.suntwister@gmail.com>
2025-08-08 10:49:52 +0100

    Added task_1 file

---
+++ week_1/task_1.py
@@ -0,0 +1,16 @@
+
+full_name = "Oyewusi samuel"
+university = "LASU"
+lga = "Ijebu-North"
+fav_food = "Amala and Ewedu"
+
+print(f'{full_name} \n {university} \n {lga} \n {fav_food}')
+
+# full_name = input("Kindly enter your name: ")
+# university = input("Kindly enter your institution: ")
+# lga = input("Kindly enter your local government area: ")
+# fav_food = input("what is your favourite food")
+# print(full_name)
+# print(university)
+# print(lga)
+# print(fav_food)
