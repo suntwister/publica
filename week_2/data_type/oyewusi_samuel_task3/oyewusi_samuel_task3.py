ae262b5f1a61c1124b5a620576ac8ac3040778e6
suntwister <s.suntwister@gmail.com>
2025-08-11 15:36:06 +0100

    Added week 2 files

---
+++ week_2/data_type/oyewusi_samuel_task3/oyewusi_samuel_task3.py
@@ -0,0 +1,68 @@
+# 1 
+surname = input("Enter your surname name: ")
+print(surname.upper())
+
+# 2
+word = "python"
+print(" first charact is", word[0],  ", \n last character is", word[-1])
+
+# 3
+name = input("Enter your name: ")
+print("Hello, !", name)
+
+4
+text = "welcome to python"
+print(text.find(text[-1]))
+
+# 5
+word_2 = "Hello world"
+new_word_2 = word_2.replace("world", "python")
+print(new_word_2)
+
+# 6 
+print("python" in new_word_2)
+
+# 7
+a ="Hello"
+print(a[-1:-6:-1])
+
+# 8
+word_2 = "  Hello world  "
+print(word_2.strip())
+
+# 9
+vowel_count = input("Enter a sentence: ")
+a = vowel_count.count("a")
+e = vowel_count.count("e")
+i = vowel_count.count("i")
+o = vowel_count.count("o")
+u = vowel_count.count("u")
+total_vowel = a+e+i+o+u
+print("The total vowel in your sentence is:", total_vowel)
+
+# 10
+string_1 = "1234"
+conv_str = int(string_1) * 2
+print(conv_str)
+
+# 11
+string_2 = "apple,banana,orange"
+print(string_2.split(","))
+
+# 12
+user_input = input("Enter a sentence: ")
+input_conv = user_input.split()
+print("\n".join(input_conv))
+
+# 13
+string_3 = "Hello user welcome"
+print(string_3.replace(" ", "_"))
+
+# 14
+string_4 = "banana"
+string_count = string_4.count("a")
+print(string_count)
+
+# 15
+string_4 = "banana"
+print("https://" in string_4)
---
+++ week_2/data_type/working_with_strings.py
@@ -0,0 +1,20 @@
+story = '''
+In the bustling heart of Lagos, there lived a weaver named Amara, known far and wide for her 
+beautiful fabrics and sharp mind. Every morning, she sat by the lagoon’s edge, weaving 
+vibrant cloths that told stories of her people: the shimmering waters, the flying fish, and 
+the lively market scenes.
+One day, the local chief announced a contest to find the cleverest person in the town. 
+The prize was a golden loom said to weave the finest cloth in all of Nigeria. Many villagers 
+rushed to participate, eager to win the prize.
+When it was Amara’s turn, the chief asked her a question: “How can you turn a thread as thin 
+as a hair into something stronger than steel?”
+Amara smiled calmly and replied, “By weaving it together with others, just as a village grows
+ stronger when its people work together.”
+'''
+
+# print(len(story))
+# print("Amara" not in story)
+print(story.find("Amara"))
+
+print(story.rfind("Amara"))
+
