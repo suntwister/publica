2b11af667c0cb8e49075e33ad1a086a081526b57
suntwister <s.suntwister@gmail.com>
2025-08-12 14:44:07 +0100

    Added task 4

---
+++ week_2/oyewusi_samuel_task4/oyewusi_samuel_task_4.py
@@ -0,0 +1,26 @@
+# # Task 1
+# input_1 = str(input("Enter your favourite quote: "))
+# # converting input
+# conv_input = input_1.title()
+# print("\"" + conv_input + "\"")
+
+# # Task 2
+# Emp_list = []
+# shop_1 = input("Enter shopping item 1: ")
+# shop_2 = input("Enter shopping item 2: ")
+# shop_3 = input("Enter shopping item 1: ")
+# Emp_list.append(shop_1)
+# Emp_list.append(shop_2)
+# Emp_list.append(shop_3)
+# New_list= list(Emp_list[0] +  Emp_list[1] + Emp_list[2])
+# print(New_list)
+
+# # Task 3
+# input_2 = str(input("Kindly enter a sentence:\n"))
+# split_input = list(input_2)
+# print(len(split_input))
+
+# Task 4
+input_3 = input("Pls enter 5 of your names seperated by space: ")
+input_3.lower()
+print(input_3)
--- week_2/working_with_list.py
+++
@@ -1,7 +0,0 @@
-# squares = [x**2 for x in range(5)]
-# print(squares)
-# evens = [x for x in range(11) if x%2 == 0]
-
-nested_list = [[1, 2], ["a", "b"]]
-print(nested_list)       # [[1, 2], ['a', 'b']]
-print(nested_list[1]) # 2
