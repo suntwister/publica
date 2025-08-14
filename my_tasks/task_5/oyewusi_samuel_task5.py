6db50e0ee06398b565a0ce07c7ea0e31e650e4b0
suntwister <s.suntwister@gmail.com>
2025-08-14 10:41:36 +0100

    New task

--- my_tasks/task_5/oyewusi_samuel_task5.py
+++ my_tasks/task_5/oyewusi_samuel_task5.py
@@ -1,22 +1,4 @@
 
-# # 4 
-# # Collecting user profiles
-# user_name = input("Enter your first name: \n").title()
-# user_age = input("Enter your age: \n")
-# user_color = input("Enter your favorite color: \n").title()
-# user_town = input("Enter your home town: \n").title()
-# # adding inputs to list and converting to tuple
-# list2 = [user_name, user_age, user_color, user_town]
-# user_profile = tuple(list2)
-# converting tuple to variable
-# users = str(user_profile)
-# print("----------------------------------")
-# print(f"First name:\t | {user_name}")
-# print(f"Age:    \t | {user_age}")
-# print(f"Favorite color:\t | {user_color}")
-# print(f"Home town:\t | {user_town}")
-# print("----------------------------------")
-
 # 5
 shop_list1 = input("Enter first item for shopping: \n").title()
 shop_list2 = input("Enter second item for shopping: \n").title()
